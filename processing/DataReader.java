0308405cb20bb69c08edf6a3bb8b62a4c3b9ad79
david <djames@34north.com>
2018-02-20 09:54:58 -0800

    reading from file instead of web yay

--- processing/DataReader.java
+++ processing/DataReader.java
@@ -2,17 +2,81 @@ import java.util.*;
 import java.io.*;
 
 public class DataReader{
-   public static void main(String[] args){
+   public static void main(String[] args) throws Exception{
       ArrayList<Place> places = new ArrayList<Place>();
+      //readGoogle(places);
+      //readYelp(places);
+      readFromFile(places);
+      //writeToFile(places);
       System.out.println(places.size());
-      readGoogle(places);
-      //System.out.println(places);
-      System.out.println(places.size());
-      readYelp(places);
-      System.out.println(places.size());
+   }
+   private static void readFromFile(ArrayList<Place> places) throws Exception{
+      Scanner scan = new Scanner(new File("places.txt"));
+      while(scan.hasNextLine()){
+         String name = scan.nextLine();
+         String address = scan.nextLine();
+         String phone = scan.nextLine();
+         String hours = scan.nextLine();
+         String price = scan.nextLine();
+         String rating = scan.nextLine();
+         String website = scan.nextLine();
+         ArrayList<String> types = new ArrayList<String>();
+         String next = scan.nextLine();
+         while(!next.equals("||")){
+            types.add(next);
+            next = scan.nextLine();
+         }
+         ArrayList<Place.Review> reviews = new ArrayList<Place.Review>();
+         next = scan.nextLine();
+         while(!next.equals("--")){
+            String text = next;
+            next = scan.nextLine();
+            while(!next.equals("^^")){
+               text+=("\n"+next);
+               next = scan.nextLine();
+            }
+            reviews.add(new Place.Review(text, scan.nextLine()));
+            next = scan.nextLine();
+         }
+         places.add(new Place(name,types,address,phone,hours,price,website,rating,reviews));
+      }
+
    }
    private static void readYelp(ArrayList<Place> places){
       Grabber.yelp(places);
+   }
+   private static void writeToFile(ArrayList<Place> places){
+      try(FileWriter fw = new FileWriter("places.txt", true);
+          BufferedWriter bw = new BufferedWriter(fw);
+          PrintWriter out = new PrintWriter(bw))
+      {
+
+         for(Place p : places){
+            out.println(p.name);
+            out.println(p.address);
+            out.println(p.phone);
+            out.println(p.hours);
+            out.println(p.price);
+            out.println(p.rating);
+            out.println(p.website);
+            for(String t : p.types){
+               out.println(t);
+            }
+            out.println("||");
+            try{
+               for(Place.Review r : p.reviews){
+                  out.println(r.text);
+                  out.println("^^");
+                  out.println(r.rating);
+               }
+            }catch(Exception e){
+            
+            }
+            out.println("--");
+         }
+      } catch (IOException e) {
+          //exception handling left as an exercise for the reader
+      }
    }
    private static void readGoogle(ArrayList<Place> places){
       try{
